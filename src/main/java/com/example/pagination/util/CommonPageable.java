975cc404c4d9085ac88341b9f6035d06fd8e63d3
HyuneyBee <gsh2448@gmail.com>
2023-12-02 23:28:48 +0900

    공용 Pageable Param 추가

---
+++ src/main/java/com/example/pagination/util/CommonPageable.java
@@ -0,0 +1,23 @@
+package com.example.pagination.util;
+
+import org.springframework.data.domain.PageRequest;
+
+public class CommonPageable {
+
+    private int page = 1;
+    private int pageSize = 10;
+
+    public void setPage(int page) {
+        this.page = page <= 0 ? 1 : page;
+    }
+
+    public void setSize(int pageSize) {
+        int DEFAULT_SIZE = 10;
+        int MAX_SIZE = 50;
+        this.pageSize = pageSize > MAX_SIZE ? DEFAULT_SIZE : pageSize;
+    }
+
+    public PageRequest of() {
+        return PageRequest.of(page - 1, pageSize);
+    }
+}
